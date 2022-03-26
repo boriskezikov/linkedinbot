d1c767884d613ab2afb6905965039fe693bc7b56
boriskezikov <bakezikov@vtb.ru>
2022-03-27 02:22:03 +0300

    add keep alive point

---
+++ src/main/java/com/tr/linkedinbot/config/KeepAliveController.java
@@ -0,0 +1,18 @@
+package com.tr.linkedinbot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class KeepAliveController {
+
+
+    @GetMapping
+    public String keepAlive() {
+        log.info("keep alive request handled");
+        return "UP";
+    }
+}
